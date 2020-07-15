7b64d34a7764237ffbe4e7b7e78c5fb0d8efdec3
Paul Edwards <paul.edwards@microsoft.com>
2020-07-15 09:32:48 +0100

    Added scripts for setting up theia

---
+++ setup/install_theia.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+apps_dir=/apps
+
+mkdir theia
+cp package.json theia
+
+pushd theia
+
+yarn --pure-lockfile && \
+    NODE_OPTIONS="--max_old_space_size=4096" yarn theia build && \
+    yarn theia download:plugins && \
+    yarn --production && \
+    yarn autoclean --init && \
+    echo *.ts >> .yarnclean && \
+    echo *.ts.map >> .yarnclean && \
+    echo *.spec.* >> .yarnclean && \
+    yarn autoclean --force && \
+    yarn cache clean
+
+cat <<EOF >launch.sh
+#!/bin/bash
+
+port=\$1
+workspace=\$2
+
+cd $apps_dir/theia
+node src-gen/backend/main.js --hostname \$HOSTNAME --port \$port "\$workspace"
+EOF
+
+popd
+
+sudo mv theia /apps/.
